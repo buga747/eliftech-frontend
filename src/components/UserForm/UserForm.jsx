3e76bb10d2389ac5e5b43aa689363e707553bbbd
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-05-27 21:18:18 +0300

    recapcha

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -67,7 +67,7 @@ const UserForm = ({ handleInputChange, handleSubmit, formData }) => {
             required
           />
         </InputContainer>
-        <ReCAPTCHA sitekey="6Ldwt0UmAAAAAECXTSdFvJHi7ZZ47ZOC2tdudZ71" />{' '}
+        <ReCAPTCHA sitekey="6Lc7zkUmAAAAANvm_ujvH1vCBpmVR2aWnXXdLhWo" />{' '}
         <ButtonContainer>
           <SubmitButton type="submit">Submit</SubmitButton>
         </ButtonContainer>
